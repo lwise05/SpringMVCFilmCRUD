791d075ac1b53957aa7f99c915a6dea45c057f01
Andrew Nelson <nels4766@gmail.com>
2024-12-13 17:13:49 -0700

    Configure controller and home jsp

---
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.film.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FilmController {
+	
+	@RequestMapping(path = {"/", "home.do"} )
+	public String goHome(Model model) {
+		return "home";
+	}
+
+}
